d76d1ceb3a8632dfe45432993fa30dd7cb05264a
Hamed <hd_sali@yahoo.com>
2023-06-27 13:51:29 +0330

    Initial commit

---
+++ Chat/ChatApp/ChatServer/Program.cs
@@ -0,0 +1,76 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using ChatServer.Net.IO;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static List<Client> _users;
+        static TcpListener _listener;   
+        static void Main(string[] args)
+        {
+            _users =new List<Client>();
+            //_listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
+            _listener = new TcpListener(IPAddress.Parse("10.1.6.125"), 9200);
+            _listener.Start();
+
+            while (true)
+            {
+              var client = new Client(_listener.AcceptTcpClient());
+              _users.Add(client);
+
+                // Broadcast the connection to everyone on the server
+                BroadcastConnection();
+            }
+
+        }
+
+        static void BroadcastConnection()
+        {
+            foreach (var user in _users)
+            {
+                foreach (var usr in _users)
+                {
+                    var broadcastPacket = new PacketBuilder();
+                    broadcastPacket.WriteOpCode(1);
+                    broadcastPacket.WriteMessage(usr.Username);
+                    broadcastPacket.WriteMessage(usr.UID.ToString());
+                    user.ClientSocket.Client.Send(broadcastPacket.GetPaketBytes());
+                }
+            }
+        }
+
+        public static void BroadcastMessage(string message)
+        {
+            foreach (var user in _users)
+            {
+                var msgPacket = new PacketBuilder();
+                msgPacket.WriteOpCode(5);
+                msgPacket.WriteMessage(message);
+                user.ClientSocket.Client.Send(msgPacket.GetPaketBytes());
+
+            }
+        }
+
+        public static void BroadcastDisconnect(string uid)
+        {
+            var disconnectedUser = _users.Where(x => x.UID.ToString() == uid).FirstOrDefault();
+            _users.Remove(disconnectedUser);
+            foreach (var user in _users)
+            {
+                var broadcastPacket = new PacketBuilder();
+                broadcastPacket.WriteOpCode(10);
+                broadcastPacket.WriteMessage(uid);
+                user.ClientSocket.Client.Send(broadcastPacket.GetPaketBytes());
+            }
+
+            BroadcastMessage($"[{disconnectedUser.Username}] Disconnected!");
+        }
+    }
+}
+
